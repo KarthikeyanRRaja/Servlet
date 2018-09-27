f3cdb07e600f0df68f6b3f847786d5825acf1d75
KarthikeyanRRaja <40626199+KarthikeyanRRaja@users.noreply.github.com>
2018-09-28 00:21:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+using System.IO;
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (File.Exists("test.txt"))
+            {
+                string content = File.ReadAllText("test.txt");
+                Console.WriteLine("Current content of file:");
+                Console.WriteLine(content);
+            }
+            Console.WriteLine("Please enter new content for the file:");
+            string newContent = Console.ReadLine();
+            File.WriteAllText("test.txt", newContent);
+        }
+    }
+}
